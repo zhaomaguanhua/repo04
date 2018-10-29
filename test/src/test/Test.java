048448094d8f82762cf259a2eb17bcc31e0e7d09
root <2488370302@qq.com>
2018-10-29 20:26:17 +0800

    增加了输出语句

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test执行了............");
         System.out.println("test执行了............");
+        System.out.println("33333333333333333333333333333");
     }
 }
