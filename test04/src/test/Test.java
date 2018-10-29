ec101ed282494f5651529b620906c3eade628b5a
root <2488370302@qq.com>
2018-10-29 21:06:54 +0800

    新建类和属性

--- test04/src/test/Test.java
+++ test04/src/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test04......................");
         System.out.println("test04......................");
+        System.out.println("hahahahhahahahahahhahahahahahahahhahaaaaaaa");
     }
 }
---
+++ test04/src/test/Test02.java
@@ -0,0 +1,5 @@
+package test;
+
+public class Test02 {
+    private int id;
+}
