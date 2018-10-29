7c8ca1b71677785994f1732b6d4a087da7ae6b0c
root <2488370302@qq.com>
2018-10-29 20:57:26 +0800

    新建一个输出语句

--- test04/src/test/Test.java
+++ test04/src/test/Test.java
@@ -3,5 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test04......................");
+        System.out.println("test04......................");
     }
 }
