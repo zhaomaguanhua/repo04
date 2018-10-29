90df0a92e28ce7045f7e3de5fca6b5357e48dd70
root <2488370302@qq.com>
2018-10-29 19:43:33 +0800

    将工程添加到远程仓库2

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,5 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test执行了............");
+        System.out.println("test执行了............");
     }
 }
